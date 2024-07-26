90f15261637e71b2f7bf0befce2a5641ad68c86c
Simon Schubert <simonlouis.schubert@gmail.com>
2024-07-26 17:14:57 +0200

    add about text

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -7,30 +7,39 @@ export default function AboutPage({ members }) {
       <div className="container mx-auto">
         <div className="mb-16 text-center lg:mb-28">
           <Typography
-            variant="h6"
-            color="blue-gray"
-            className="font-futura text-lg"
-          >
-            Meet the Team
-          </Typography>
-          <Typography
             variant="h1"
             color="blue-gray"
             className="my-2 font-futura !text-2xl lg:!text-4xl"
           >
-            Behind the Success: Our Dedicated Team
+            About the project
           </Typography>
           <Typography
             variant="lead"
             className="mx-auto w-full !text-gray-500 max-w-4xl"
           >
-            From visionary leadership to creative talent, and technical wizards,
-            each team member plays a pivotal role in delivering the exceptional
-            service and innovative solutions.
+            Our project is a dynamic blogging platform designed to offer a
+            seamless user experience for both reading and interacting with blog
+            content. Users can browse through various blog posts and filter them
+            by category for a more personalized experience. Each blog post
+            allows users to leave comments, fostering engagement and discussion.
+            For our backend, we utilized Strapi, which enabled us to efficiently
+            create and manage the blog fields, ensuring a robust and flexible
+            content management system. This setup allows for easy scalability
+            and customization, enhancing the overall functionality and user
+            experience of the blog.
           </Typography>
         </div>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-          <TeamList members={members} />
+        <div className="flex-col items-center justify-center">
+          <Typography
+            variant="h6"
+            color="blue-gray"
+            className="font-futura text-center mb-10 text-xl"
+          >
+            Meet the Team
+          </Typography>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            <TeamList members={members} />
+          </div>
         </div>
       </div>
     </section>
